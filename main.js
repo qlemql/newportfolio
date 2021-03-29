2ae706d0736e754eb205f35b4dc1bf68f9508357
qlemql <busgamer@naver.com>
2021-03-29 23:23:05 +0900

    Create About, Add Sidebar
    Add sidebar when toggle click swich section
    
    Create about scroll animation with css & js

--- main.js
+++ main.js
@@ -1,6 +1,45 @@
+
+// TOGGLE MENU
 function toggleMenu(){
     const menuToggle = document.querySelector('.toggle');
     const sidebar = document.querySelector('.sidebar');
     menuToggle.classList.toggle('active');
     sidebar.classList.toggle('active');
 }
+
+// SCROLL
+
+const animeDefaultMargin = 200;
+let animeTriggerMargin = 0;
+let animeTriggerHeight = 0;
+const animeElementList = document.querySelectorAll(".anime");
+
+const animeFunc = function() {
+    for (const element of animeElementList) {
+      if (!element.classList.contains('show')) {
+        if (element.dataset.animeMargin) {
+            animeTriggerMargin = parseInt(element.dataset.animeMargin);
+        } else {
+            animeTriggerMargin = animeDefaultMargin;
+        }
+  
+        if (element.dataset.animeTrigger) {
+            animeTriggerHeight = document.querySelector(element.dataset.animeTrigger).getBoundingClientRect().top + animeTriggerMargin;
+        } else {
+            animeTriggerHeight = element.getBoundingClientRect().top + animeTriggerMargin;
+        }
+  
+        if (window.innerHeight > animeTriggerHeight) {
+          let delay = (element.dataset.animeDelay) ? element.dataset.animeDelay : 0;
+          setTimeout(function() {
+            element.classList.add('show');
+          }, delay);
+        }
+      }
+    }
+  }
+
+window.addEventListener("load", animeFunc);
+window.addEventListener('scroll', animeFunc);
+
+
